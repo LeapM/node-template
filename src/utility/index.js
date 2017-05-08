4e46971b91b5530598db911e70b6b85e50bbcd44
Luming Wang <lum.wang@gmail.com>
2017-05-08 16:10:23 +0800

    add utility

---
+++ src/utility/index.js
@@ -0,0 +1,8 @@
+import chalk from 'chalk'
+export function logError(msg){
+
+  console.log(chalk.bold.red(arguments.callee.caller + ": " +msg));
+}
+export function logDebug(msg){
+  console.log(msg);
+}
