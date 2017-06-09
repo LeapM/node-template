a1e78bf6644d487128fbf47a3d96019c9416958a
Wang <Luming.Wang@jacobs.com>
2017-06-09 16:49:48 +0800

    add body-parser support

--- src/app.js
+++ src/app.js
@@ -6,9 +6,19 @@ import "regenerator-runtime/runtime";
 dotenv.config();
 const app = express();
 const port = process.env.port || 3000;
+//for cross origin scripting
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+//add body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/',(req,res)=> res.send('hello world'));
 
-app.listen(port,()=>{
-  console.log(`listen on localhost:${port}`);
+app.use('/', (req, res) => res.send('hello world'));
+
+app.listen(port, () => {
+	console.log(`listen on localhost:${port}`);
 })
