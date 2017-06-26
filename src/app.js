e9b8f5d813be835d3e990f201c155f5a0f18d154
Wang <Luming.Wang@jacobs.com>
2017-06-26 14:31:33 +0800

    add body parser

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
 //make regenerratorruntime globally defined
 import "regenerator-runtime/runtime";
 
